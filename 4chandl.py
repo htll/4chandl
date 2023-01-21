b9eab9443621f198229de668572aa3b565986668
mouseroot <mouseroot@gmail.com>
2023-01-21 06:12:02 -0600

    Updated code for Python3, Repaired script.

--- 4chandl.py
+++ 4chandl.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-import urllib
-import urllib2
+import requests
+import os.path
 import os
 from optparse import OptionParser
 from time import sleep
@@ -8,50 +8,60 @@ from time import sleep
 def fetchImages(url):
 	downloaded_images = []
 	#Our user-agent
-	user_agent = "Mozzila/6 (Windows 7 64bit) Gecko"
+	user_agent = "Mozzila/9 (Windows 10 64bit) Gecko"
 	headers = {"user-agent": user_agent}
 
 	#Request/Response
-	request = urllib2.Request(url,None,headers)
-	response = urllib2.urlopen(request)
-	page = response.read()
+	request = requests.get(url,allow_redirects=True,headers=headers)
+	if request.status_code != 200:
+		return
+	else:
+		#Parse response into HTMLEntities
+		pool = BeautifulSoup(request.text)
 
-	#Parse response into HTMLEntities
-	pool = BeautifulSoup(page)
-
-	#Search for all image "links"
-	images =  pool.findAll("a",attrs={"class":"fileThumb"})
-	for image in images:
-
-		#Append all links to our output list
-		downloaded_images.append("http:%s" % image["href"])
+		#Search for all image "links"
+		images =  pool.select("a[class=fileThumb]")
+		for image in images:
+			href = image.attrs['href']
+			print(f"Found {href}")
+			#Append all links to our output list
+			downloaded_images.append(f"http:{href}")
 	return downloaded_images
+
+def save_file(url, output_folder, filename):
+	print(f"Downloading {filename}")
+	with open(f"{output_folder}/{filename}","wb") as ifile:
+		r_image = requests.get(url)
+		ifile.write(r_image.content)
 
 
 def main(url,folder=None,sleep_time=None):
-	print "Downloading from",url
+	print(f"Attempting to download from {url}")
 
 	#Check if the folder exists
 	if os.path.exists(folder):
-		print "Folder exists"
+		print(f"Folder {folder} exists")
 	else:
-		print "Createing folder",folder
+		print(f"Createing folder {folder}")
 		os.mkdir(folder)
 
 	#Fetch images
 	images = fetchImages(url)
-	for dlImage in images:
+	for index,item in enumerate(images):
 
 		#extract the filename by splitting the last /
-		filename = dlImage.split("/")[-1:]
-		print "Downloading",filename[0]
+		filename = item.split("/")[-1:][0]
 
+		print(f"Item {index}/{len(images)}")
 		#download the image
-		urllib.urlretrieve(dlImage,"%s/%s" %(folder,filename[0]))
-		
-		#if a sleep time was specified then sleep 
-		if sleep_time:
-			sleep(sleep_time)
+		save_file(item,folder,filename)
+
+		#if a sleep time was specified then sleep
+		try:
+			if sleep_time:
+				sleep(sleep_time)
+		except KeyboardInterrupt:
+			break
 
 
 
